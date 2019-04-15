a10a07464ebf3f717ff6b10bf1f128d2f76d7dfb
zsw <zsw0407@outlook.com>
2019-04-15 19:18:25 +0800

    添加网站LOGO && 添加首页headbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-
+    <div class="home_header">
+      <div class="home_logobar">
+        <img src="../assets/logoicon.png" alt="" height="70px" width="70px">
+        <img src="../assets/logotext.png" alt="" class="logotext">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,9 +16,28 @@ export default {
   components: {
 
   },
-  created() {
-    //登录主页面创建时初始化cookie
+  created () {
+    // 登录主页面创建时初始化cookie
     initToken()
-  },
+  }
 }
 </script>
+
+<style scoped>
+  .home_header {
+    width: 100%;
+    height: 80px;
+    border-bottom: 2px solid lightskyblue;
+  }
+  .home_logobar {
+    margin: 5px auto;
+    padding: 5px 0;
+    width: 750px;
+    height: 70px;
+  }
+  .logotext {
+    width: 420px;
+    height: 50px;
+    margin: 10px 130px;
+  }
+</style>
