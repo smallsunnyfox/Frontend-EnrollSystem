2f63624ccc4e059c4d328f1b8c0fecc3baa7a755
zsw <zsw0407@outlook.com>
2019-05-10 00:02:40 +0800

    完成报名表单的生成和报名信息的查看

--- src/views/participant/activity/signupActivity.vue
+++ src/views/participant/activity/signupActivity.vue
@@ -30,11 +30,36 @@
     <br>
     <br>
     <br>
+    <el-alert
+      v-show="couldSignupActivities.length === 0"
+      title="暂无可报名的活动"
+      type="success"
+      :closable="false"
+      style="margin-bottom:10px;"
+    ></el-alert>
     <el-button icon="el-icon-refresh" type="primary" size="small" circle title="刷新活动" @click="refreshData()"></el-button>
     <!-- 查看活动信息的Dialog -->
     <el-dialog :visible.sync="activityInfoDialog" width="50%" top="58px">
       <!-- title -->
       <div slot="title" class="dialog-title"><i class="el-icon-document"></i> 活动信息</div>
+      <el-form label-position="left">
+        <el-form-item>
+          <span slot="label"><i class="el-icon-location-outline"></i>活动地点:</span>
+          <span style="color:gray;float:left;">{{ moreInfo.site }}</span>
+        </el-form-item>
+        <el-form-item>
+          <span slot="label"><i class="el-icon-time"></i>活动开始时间:</span>
+          <span style="color:gray;float:left;">{{ moreInfo.starttime }}</span>
+        </el-form-item>
+        <el-form-item>
+          <span slot="label"><i class="el-icon-time"></i>活动结束时间:</span>
+          <span style="color:gray;float:left;">{{ moreInfo.endtime }}</span>
+        </el-form-item>
+        <el-form-item>
+          <span slot="label"><i class="el-icon-collection-tag"></i>活动详情:</span>
+          <span style="color:gray;float:left;text-align:left;">{{ moreInfo.detail }}</span>
+        </el-form-item>
+      </el-form>
       <!-- footer -->
       <div slot="footer">
         <el-button @click="activityInfoDialog = false">关闭查看</el-button>
@@ -44,6 +69,25 @@
     <el-dialog :visible.sync="signupDialog" width="50%" top="58px">
       <!-- title -->
       <div slot="title" class="dialog-title"><i class="el-icon-document-add"></i> 活动报名</div>
+      <el-form label-width="auto" label-position="right">
+        <el-form-item v-for="item in signupEntryItems" :key="item.id" :label="item.name">
+          <el-input v-if="item.type === 'input'" v-model="signupEntryForm[item.name]"></el-input>
+          <el-input v-else-if="item.type === 'textarea'" type="textarea" v-model="signupEntryForm[item.name]"></el-input>
+          <el-input v-else-if="item.type === 'inputnumber'" type="number" v-model="signupEntryForm[item.name]"></el-input>
+          <el-radio-group v-else-if="item.type === 'radio'" v-model="signupEntryForm[item.name]" style="float:left;margin-top:13px;">
+            <el-radio v-for="option in item.options.split(',')" :key="option" :label="option">{{option}}</el-radio>
+          </el-radio-group>
+          <el-checkbox-group v-model="signupEntryForm[item.name]" v-else-if="item.type === 'checkbox'" :value="[]" style="float:left;height:40px;">
+            <el-checkbox v-for="option in item.options.split(',')" :key="option" :label="option">{{option}}</el-checkbox>
+          </el-checkbox-group>
+          <el-date-picker style="float:left;" v-model="signupEntryForm[item.name]" v-else-if="item.type === 'datepicker'" :placeholder="'选择'+item.name"></el-date-picker>
+          <el-select :value="''" style="float:left;" v-model="signupEntryForm[item.name]" v-else-if="item.type === 'select'" placeholder="请选择">
+            <el-option v-for="option in item.options.split(',')" :key="option" :label="option" :value="option"></el-option>
+          </el-select>
+          <br v-if="item.reminder!=='' && item.reminder!==null">
+          <span v-if="item.reminder!=='' && item.reminder!==null" style="font-size:12px;color:gray;float:left;">填写提示：{{item.reminder}}</span>
+        </el-form-item>
+      </el-form>
       <!-- footer -->
       <div slot="footer">
         <el-button @click="signupDialog = false">取消报名</el-button>
@@ -55,11 +99,17 @@
 
 <script>
 import Vue from 'vue'
-import { getCouldSignupActivity, searchCouldSignupActivity } from '@/api/activity.js'
-import { Select, Option, Message, ButtonGroup } from 'element-ui'
+import { getCouldSignupActivity, searchCouldSignupActivity, getEntryItemsOfActivity } from '@/api/activity.js'
+import { Select, Option, Message, ButtonGroup, Alert, Radio, RadioGroup, CheckboxGroup, Checkbox, DatePicker } from 'element-ui'
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(ButtonGroup)
+Vue.use(Alert)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(DatePicker)
 export default {
   name: 'signupActivity',
   data () {
@@ -68,7 +118,15 @@ export default {
       searchSelectValue: 'name',
       couldSignupActivities: [],
       activityInfoDialog: false,
-      signupDialog: false
+      signupDialog: false,
+      moreInfo: {
+        site: '',
+        starttime: '',
+        endtime: '',
+        detail: ''
+      },
+      signupEntryItems: [],
+      signupEntryForm: {}
     }
   },
   created () {
@@ -131,14 +189,39 @@ export default {
       this.getCouldSignupActivity()
     },
     viewActivityInfo (activity) {
-      console.log(activity)
+      this.moreInfo.site = activity.site
+      this.moreInfo.starttime = activity.starttime
+      this.moreInfo.endtime = activity.endtime
+      this.moreInfo.detail = activity.detail
       this.activityInfoDialog = true
     },
     signupActivity (entryform) {
-      console.log(entryform)
-      this.signupDialog = true
+      const loading = this.$loading({
+        lock: true,
+        text: '生成报名表单中',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      })
+      setTimeout(() => {
+        getEntryItemsOfActivity(entryform)
+          .then(response => {
+            this.signupEntryItems = response.data
+            this.signupEntryForm = {}
+            for (var i = 0; i < this.signupEntryItems.length; i++) {
+              if (this.signupEntryItems[i].type === 'checkbox') {
+                this.$set(this.signupEntryForm, this.signupEntryItems[i].name, [])
+              } else {
+                this.$set(this.signupEntryForm, this.signupEntryItems[i].name, '')
+              }
+            }
+            loading.close()
+            this.signupDialog = true
+          }).catch(error => {
+            console.log(error)
+          })
+      }, 500)
+      // Vue.$set([想要添加字段的对象],'字段名',字段值)
     }
-
   }
 }
 </script>
